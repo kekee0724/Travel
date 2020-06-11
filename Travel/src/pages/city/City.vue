29b900984f13da9ba850b04b9084ae7d7e9b84b6
可可同学 <androidleo@outlook.com>
2020-06-11 10:55:03 +0800

    search finish

--- Travel/src/pages/city/City.vue
+++ Travel/src/pages/city/City.vue
@@ -2,7 +2,7 @@
     <div>
       <city-header></city-header>
       <div id="city-container">
-        <city-search></city-search>
+        <city-search  :cities="cities"></city-search>
         <city-list :cities="cities" :hot="hotCities" :letter="letter"></city-list>
         <city-alphabet :cities="cities" @change="handleLetterChange"></city-alphabet>
       </div>
--- Travel/src/pages/city/components/Search.vue
+++ Travel/src/pages/city/components/Search.vue
@@ -7,16 +7,60 @@
       </ul>
   </div>
   <div class="search">
-  <!-- <div class="search-input" > -->
-    <input class="search-input" type="text" placeholder="输入城市名或拼音"/>
-  <!-- </div> -->
+    <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音"/>
+  </div>
+  <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+          <li class="search-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+          <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配城市</li>
+      </ul>
   </div>
 </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.search)
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          if (this.cities.hasOwnProperty(i)) {
+            this.cities[i].forEach((value) => {
+              if (value.spell.indexOf(this.keyword) > -1 ||
+              value.name.indexOf(this.keyword) > -1) {
+                result.push(value)
+              }
+            })
+          }
+          this.list = result
+        }
+      }, 100)
+    }
+  }
 }
 </script>
 
@@ -52,4 +96,18 @@ export default {
       border-radius .06rem
       color #666
       background-color: #fff
+  .search-content
+    z-index 1
+    overflow hidden
+    position absolute
+    top 2.38rem
+    left 0
+    right 0
+    bottom 0
+    background #eee
+    .search-item
+      line-height .62rem
+      padding-left .2rem
+      background #fff
+      color #666
 </style>
